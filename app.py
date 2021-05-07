c99455a812f42fc8ff93ae47110e599b1b042be8
Shaon Sikder <shaonthegreat.wellwisher@gmail.com>
2021-05-08 02:36:27 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+# importing libraries 
+import speech_recognition as sr 
+import os 
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from flask import Flask, render_template, request, redirect
+
+
+# create a speech recognition object
+r = sr.Recognizer()
+
+# Function for transcribe(a function that splits the audio file into chunks and applies speech recognition)
+
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)  
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk 
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            #recognizer = sr.Recognizer()
+            #audioFile = sr.AudioFile(file)
+            #with audioFile as source:
+                #data = recognizer.record(audioFile)
+            sound = AudioSegment.from_mp3(file)
+            sound.export("transcript.wav", format="wav")
+            transcript = get_large_audio_transcription("transcript.wav")
+
+    return render_template('index.html', transcript=transcript)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
